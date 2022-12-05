2290991753967027313377da478616f43c5bd171
Keigo Yoshida <103874477+Kyle1143@users.noreply.github.com>
2022-12-05 09:17:07 +0900

    Add files via upload

---
+++ study/asarin.py
@@ -0,0 +1,184 @@
+# ここのpylanceのエラーは一旦無視
+import numpy as np
+import matplotlib.pylab as plt
+import sys
+
+# デバッガーを使う
+# set_trace()をおくと，その行で止まる
+from pdb import set_trace
+
+
+# from mnist import load_mnist # load_mnistファイルを実行
+
+#
+# 1.パラメータの初期値(2層)
+# w = (784, 10)
+# b = (b1, b2)
+
+# 2.必要な関数を定義
+# softmax関数#
+# TODO: ここ埋める
+# 入力 x: ベクトル,　どんな意味を持っている？
+# 出力　y: ベクトル,　y_0=...、y_1=...、
+# 変数名は適当につけない方
+# 正しいかどうか実際に実行してみてみよう
+# 全体を実行するのではなく，この関数だけ切り出してみる
+# 完成！
+
+# 一つにしか対応していない -> 行列に対応していない
+def softmax(x):
+    y = np.zeros_like(x)
+    for i in range(x.shape[0]):
+        a = np.max(x[i])  # a：入力xの最大値 OK
+        exp1 = np.exp(x[i]-a)  # exp1：入力xの最大値から引く
+        y[i] = exp1 / np.sum(exp1)
+
+    return y
+
+
+# 2-1predict(予測値)
+# x(100, 784), w(784, 10), 欲しい形(100, 10)
+# 100: バッチサイズ, 784: 特徴量(28*28)
+# 入力 x:100のバッチサイズ、784の特徴量 (100x784の行列)
+# 出力 y2:100のバッチサイズ、10の各ラベルの予測の確率 y2_0 = (0.1, 0.01, 0, 0.7, 0.02,....)
+
+def predict(x):
+    # ランダムに行列を生成
+    # w = np.random.rand(784, 10)
+
+    # 入力画像の想定
+    # x.shape = (ミニバッチ，1つのサンプルの特徴量の数)
+    # w: モデルの重み
+    # w.shape = (1つのサンプルの特徴量の数,クラス数)
+
+    # 手計算用
+    w1 = [[0.1, 0.8, 0.4], [0.1, 0.8, 0.3], [0.3, 0.8, 0.1], [0.1, 0.8, 0.4], [
+        0.1, 0.8, 0.3], [0.3, 0.8, 0.1], [0.1, 0.8, 0.4], [0.1, 0.8, 0.3], [0.3, 0.8, 0.1]]
+    w = np.array(w1)
+
+    # ipdbはいいぞ
+    # FIX: x.shape=(3,9), w.shape=(9,3)でnp.dotと行列計算が合わないからエラー
+    # y1がどういうになるべきか -> y1.shape = (3,3), (ミニバッチ数, 各クラスの値)
+    y1 = np.dot(x, w)
+    # softmax完成!
+    # y2.shape (3,3)
+    # 各行の和が1になる
+    # 非負になるはず
+
+    y2 = softmax(y1)
+    assert all([round(sum(a)) == 1 for a in y2]), 'sum error'
+    assert all([a >= 0 for a in y2.reshape(-1)]), 'hihu error'
+    return y2
+
+
+# 2-2 cross-entropy-error(yがニューラルネットの出力、tが教師データ)
+# y(100, 10), t(100, 10)
+# 100: バッチサイズ, 10:正解ラベルの種類
+# 入力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 入力 y:100のバッチサイズ、10個の正解ラベルの種類 0<=y<=1 (100x10の行列) y = (0.1, 0.01, 0, 0.7, 0.02,....)
+# y_(0~9)の総和は1
+# 入力 t:100のバッチサイズ、10個の正解ラベルの種類 (100x10の行列) t0 = (0,0,0,1,0,0,0,...)
+# tはone-hot
+# 出力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 出力 スカラー  cross-entropyが1を超えるのは考えられる
+# 正しい出力の例を用意して、対応する入力を入れた時にそれが出力されるかを確認する
+# 例：ret = 0.8とか、ret = 1.3とか
+# 0 < retは絶対
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    # batch_size = 3 #確認用
+    delta = 1e-7  # logの中をゼロにしたくないので微小な値を入れておく
+    # batch_size=5の時、np.arange(batch_size)は[0,1,2,3,4]
+    # print(np.arange(batch_size))
+
+    # xの範囲は1.0を超えることは容易に考えられる
+    # log e^(-1)の時、-1を取るため
+    # x = t * np.log(y + delta)
+    # print(np.round(x, 2))
+
+    # 正解データtがone-hotの場合(*今回 t = 0,0,0,1,0....)
+    ret = - np.sum(t * np.log(y + delta)) / batch_size
+
+    # 正解データtが正解ラベル(ベクトル)の場合(t = 0~9までの数値)
+    # ret = -np.sum(np.log(y[np.arange(batch_size), t] + delta)) / batch_size
+    return ret
+
+
+# 2-3損失関数(xが入力画像、tが正解データ)
+# x(100, 784), t(100, 10)
+# 100: バッチサイズ, 784: 28*28画像サイズ ,10:正解ラベルの種類
+# 入力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 入力 x:100のバッチサイズ、28*28画像サイズ (100x784の行列)
+# 入力 t:100のバッチサイズ、10個の正解ラベルの種類 (100x10の行列)　t0 = (0,0,0,1,0,0,0,...)
+# tはone-hot
+# 出力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 出力 cross-entropy(スカラー)
+# 出力：予測値yと正解tとの誤差を計算
+# 正しい出力の例を用意して、対応する入力を入れた時にそれが出力されるかを確認する
+
+def loss_function(x, t):
+    y = predict(x)  # x:100*784
+    # y：100*10  (0.1, 0.5, 0.05, ・・・)
+    #assert y.shape == (100, 10), 'predict shape error'
+    # 手計算用
+
+    return cross_entropy_error(y, t)  # スカラー
+
+
+# 2-4 数値微分(fが微分したい関数、xがそれで微分したい変数, tが正解データ)
+# 入力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 入力 f:微分したい関数(後々損失関数を微分したい)
+# 入力 x:それについて微分する(x0,x1,..など)
+# 入力 t:100のバッチサイズ、10個の正解ラベルの種類 (100x10の行列)　t0 = (0,0,0,1,0,0,0,...)
+# tはone-hot
+# 出力が満たすべき条件を列挙して、それを満たしているかを確認する
+# 出力 勾配 100✖️784行列
+# 正しい出力の例を用意して、対応する入力を入れた時にそれが出力されるかを確認する
+
+def numerical_gradient(f, x, t):  # 引数：損失関数、初期値(x0やx1などの値)、正解データt
+    h = 1e-4
+    grad = np.zeros_like(x)  # xと同じ形状の配列を生成(要素が全て0)
+
+    for idx in range(x.shape[0]):
+        tmp_val = x[idx]
+        # f(x+h)の計算
+        x[idx] = tmp_val + h
+        fxh1 = f(x, t)
+
+        # set_trace()
+
+        # f(x-h)の計算
+        x[idx] = tmp_val - h
+        fxh2 = f(x, t)
+        grad[idx] = (fxh1 - fxh2)/(2*h)
+
+        # x[idx] = tmp_val #値を元に戻す(for文の最初に関係がある？)
+    return grad
+
+
+# # 3.データをロード
+# (x_train, t_train), (x_test, t_test) = load_mnist(normalize = True, one_hot_label = True)
+
+# # 4.ハイパラを作成(iter、train_size、test_size、学習率)
+# iter = 100
+# train_size = x_train.shape[0]
+# lr = 0.01
+# batch_size = 100
+
+# # 5-1.ミニバッチを作成
+# for i in range(iter):
+#   a = np.random.choice(train_size, batch_size)
+#   x_batch = x_train[a] #画像 100×784 (28*28=784)
+#   t_batch = t_train[a] #画像のラベル 100×10
+
+# # 5-2.勾配の計算　
+#   # y = loss_function(x_batch, t_batch) #損失値
+#   grad = numerical_gradient(loss_function, x_batch)
+
+# # 5-3.パラメータを更新
+#   w -= grad * lr
---
+++ study/test_func.py
@@ -0,0 +1,201 @@
+# テストしたい関数を呼び出す
+import numpy as np
+
+from asarin import softmax
+from asarin import predict
+from asarin import cross_entropy_error
+from asarin import loss_function
+from asarin import numerical_gradient
+
+# def test_softmax():
+#     # 入力xを作る
+#     # 10個の数値がベクトルになっている x=[1,10,100,...,]みたいな
+#     x = np.array([10**(i+1) for i in range(10)])
+#     print(f'x.shape:{x.shape}, x:{x}')
+#     # TODO: yの中身説明する
+#     # 出力y：10(size)次元のベクトル
+#     y = softmax(x)
+#     print("y.shape: {}, y: {}".format(y.shape, y))
+
+#     # 入力xを作る
+#     # 10個の数値がベクトルになっている x=[10,20,30,...,]みたいな
+#     x = np.array([10*(i+1) for i in range(10)])
+#     print(f'x.shape:{x.shape}, x:{x}')
+#     # TODO: yの中身説明する
+#     # 出力y：10(size)次元のベクトル
+#     y = softmax(x)
+#     print("y.shape: {}, y: {}".format(y.shape, y))
+
+#     # 入力xを作る
+#     # 個の数値がベクトルになっている x=[1,2,3,4,..]みたいな
+#     x = np.array([1, 2, 1, -1, 3, 2])
+#     print(f'x.shape:{x.shape}, x:{x}')
+#     # TODO: yの中身説明する
+#     # 出力y：ベクトル(要素6)
+#     y = softmax(x)
+#     print("y.shape: {}, y: {}".format(y.shape, y))
+
+
+# def test_predict():
+#     # 入力xを作る
+#     # x = (100, 784)
+#     # x = [[ i for i in range(100)] for _ in range(784)]
+#     # ランダムに行列を生成
+#     x = np.random.rand(100, 784)
+
+#     # assert 条件式　を使うと確認が楽
+#     assert x.shape == (100,784), print('Shape Size Error')
+
+#     # print("x.shape: {}, x: {}".format(x.shape, x))
+#     y = predict(x)
+#     assert y.shape == (100,10), print('Shape Error')
+#     # print("y.shape: {}, y: {}".format(y.shape, y))
+#     # print("predict:", y)
+
+
+# def test_cross_entropy_error():
+#     # 予測値 0~1の範囲の実数 100✖️10の行列
+#     y = np.random.rand(100, 10)
+#     assert y.shape == (100, 10), print("shape error01")
+#     # print("y.shape: {}, y: {}".format(y.shape, y))
+
+#     # yの行の総和の逆数でその行の成分を割りたい -> 各行の確率の総和が1になる
+#     # ex:各行の総和
+#     # pre：確率に正規化
+#     ex = np.sum(y, axis=0)
+#     pre = y / ex
+
+#     # 正解ラベル 正解のラベルのインデックスに1、それ以外０(one-hot)　100✖️10行列
+#     # aは100の正解ラベル
+
+#     # 書き方が間違い?
+#     # a = np.random.randint(0, 9) for i in range(100)
+
+#     a = np.random.randint(10,size=(100))
+
+#     # a_list = []
+#     # for k in range(100):
+#     #     x = np.random.randint(0,9)
+#     #     a_list.append(x)
+#     # aa = np.array(a_list)
+#     # a = aa.astype(int)
+
+#     # print("a.shape: {}, a: {}".format(a.shape, a))
+#     assert a.shape == (100, ), print("shape error02")
+#     # a.ndim = 1
+#     # print(type(a))
+#     # <class 'numpy.ndarray'>
+#     a_one_hot = np.identity(10)[a]
+#     assert a_one_hot.shape == (100, 10), print("shape error03")
+#     t = a_one_hot
+#     # print("t.shape: {}, t: {}".format(t.shape, t))
+#     assert t.shape == (100, 10), print("shape error04")
+#     # print(type(t))
+#     # <class 'numpy.ndarray'>
+
+#     # one-hotの確認assert
+#     assert all([sum(b)==1 for b in t]), print('one-hot error05')
+
+#     # # print(y.ndim)
+#     # # ndim = 2
+
+#     if pre.ndim == 1:
+#         t = t.reshape(1, t.size)
+#         pre = pre.reshape(1, pre.size)
+
+
+#     # # ここだけ手計算用　
+#     # #-0.69314718055995 + -0.35667494393873 + -0.22314355131421
+#     # # 0.42431667
+#     # y1 = [[0.5, 0.4, 0.1],[0.7, 0.2, 0.1],[0.1, 0.8, 0.1]]
+#     # t1 = [[1, 0, 0],[1, 0, 0],[0, 1, 0]]
+#     # y = np.array(y1)
+#     # t = np.array(t1)
+
+#     c = cross_entropy_error(y, t)
+#     assert c.shape == (), 'Shape Error'
+#     print("c.shape: {}, cross-entropy: {}".format(c.shape, c))
+
+#     # #一旦ここまでテスト
+#     # return
+
+
+# def test_loss_function():
+    # # 入力xを作る
+    # # ランダムに行列を生成
+    # x = np.random.rand(100, 784)
+    # assert x.shape == (100,784), print('Shape Size Error')
+
+    # # 入力tを作る
+    # a = np.random.randint(10,size=(100))
+    # # print("a.shape: {}, a: {}".format(a.shape, a))
+    # assert a.shape == (100, ), print("shape error02")
+    # a_one_hot = np.identity(10)[a]
+    # assert a_one_hot.shape == (100, 10), print("shape error03")
+    # t = a_one_hot
+    # # print("t.shape: {}, t: {}".format(t.shape, t))
+    # assert t.shape == (100, 10), print("shape error04")
+    # # one-hotの確認assert
+    # assert all([sum(b)==1 for b in t]), print('one-hot error05')
+
+    # ここだけ手計算用　
+    # x1 (3✖️9行列)
+    # x1 = [[0.1, 0.8, 0.1, 0.1, 0.8, 0.1, 0.1, 0.8, 0.1], [0.1, 0.1, 0.1, 0.1,
+    #                                                       0.1, 0.1, 0.1, 0.2, 0.1], [0.1, 0.1, 0.1, 0.1, 0.2, 0.1, 0.1, 0.1, 0.1]]
+    # t1 = [[1, 0, 0], [1, 0, 0], [0, 1, 0]]
+    # x = np.array(x1)
+    # assert x.shape == (3, 9), "x shape error"
+    # t = np.array(t1)
+    # assert t.shape == (3, 3), "t shape error"
+
+    # L = loss_function(x, t)
+    # assert isinstance(L, float), 'Shape Error'
+    # print("L.shape: {}, loss-function: {}".format(L.shape, L))
+
+
+def test_numerical_gradient():
+    # # 入力xを作る
+    # # ランダムに行列を生成
+    # x = np.random.rand(100, 784)
+    # assert x.shape == (100,784), print('x Shape Size Error')
+
+    # # 入力tを作る
+    # a = np.random.randint(10,size=(100))
+    # # print("a.shape: {}, a: {}".format(a.shape, a))
+    # assert a.shape == (100, ), print("shape error02")
+    # a_one_hot = np.identity(10)[a]
+    # assert a_one_hot.shape == (100, 10), print("shape error03")
+    # t = a_one_hot
+    # # print("t.shape: {}, t: {}".format(t.shape, t))
+    # assert t.shape == (100, 10), print("shape error04")
+    # # one-hotの確認assert
+    # assert all([sum(b)==1 for b in t]), print('one-hot error05')
+
+    # # numerical_gradient(関数(loss), 微分したい変数(x))
+    # y = numerical_gradient(loss_function, x, t)
+    # assert y.shape == (100,784), print('y Shape Size Error')
+    # print("y.shape: {}, numerical_gradient: {}".format(y.shape, y))
+
+    # ここだけ手計算用　
+    # x1 (3✖️9行列)
+    x1 = [[0.1, 0.8, 0.1, 0.1, 0.8, 0.1, 0.1, 0.8, 0.1], [0.1, 0.1, 0.1, 0.1,
+                                                          0.1, 0.1, 0.1, 0.2, 0.1], [0.1, 0.1, 0.1, 0.1, 0.2, 0.1, 0.1, 0.1, 0.1]]
+    t1 = [[1, 0, 0], [1, 0, 0], [0, 1, 0]]
+    x = np.array(x1)
+    assert x.shape == (3, 9), "x shape error"
+    t = np.array(t1)
+    assert t.shape == (3, 3), "t shape error"
+
+    # numerical_gradient(関数(loss), 微分したい変数(x))
+    y = numerical_gradient(loss_function, x, t)
+    assert y.shape == (3,9), print('y Shape Size Error')
+    print("y.shape: {}, numerical_gradient: {}".format(y.shape, y))
+
+
+if __name__ == "__main__":
+    # test_softmax()
+    # test_predict()
+    # test_cross_entropy_error()
+    # test_loss_function()
+    test_numerical_gradient()
+    # pass
